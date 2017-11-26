f0f84dc79ff994e97f7fa8527387d831d17d1f04
ivanh96 <ivanh96@hotmail.com>
2017-11-25 18:07:37 -0600

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,5 +1,11 @@
+import itertools
 import random as rand
-import numpy as np
+import simpy
+
+
+maxlatas = 200     # latas totales en la maquina
+intervalo = [30, 300]        # generar una persona entre [min,max] segundos
+ttotal = 2000            # tiempo total en segundos
 
 # funcion bebida
 # devuelve un valor entre 0 y 8 para asignar bebida siguiendo la distribucion planteada
@@ -54,7 +60,34 @@ def genero():
     if r >= 0.4120603015 and r < 1:
         return 1
     
-for i in range(0,100):
-    print('bebida',bebida())
-    print('edad',edad())
-    print('genero',genero())
+def persona(nombre, env, dispensadora, bebida1):
+    print('%s arriving at gas station at %.1f' % (nombre, env.now))
+    with dispensadora.request() as req:
+        start = env.now
+        # Get the required amount of fuel
+        liters_required = 1
+        yield bebida1.get(liters_required)
+
+        # The "actual" refueling process takes some time
+        yield env.timeout(liters_required)
+
+        print('%s finished refueling in %.1f seconds.' % (nombre, env.now - start))
+
+def generar_persona(env, dispensadora, bebida1):
+    for i in itertools.count():
+        yield env.timeout(rand.randint(*intervalo))
+        nombre = 'genero'+repr(genero())+'edad'+repr(edad())
+        env.process(persona(nombre, env, dispensadora, bebida1))
+
+
+# Setup and start the simulation
+print('Gas Station refuelling')
+
+# Create environment and start processes
+env = simpy.Environment()
+dispensadora = simpy.Resource(env, 1)
+bebida1 = simpy.Container(env,maxlatas, init= maxlatas)
+env.process(generar_persona(env, dispensadora, bebida1))
+
+# Execute!
+env.run(until=ttotal)
